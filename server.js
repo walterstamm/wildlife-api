122f8eb90cddd5b1347b38d61704442f1a142d4c
wstammSeidor <walterstamm92@gmail.com>
2024-12-02 18:09:18 -0300

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const port = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
