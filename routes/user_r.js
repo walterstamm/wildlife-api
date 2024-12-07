be372a27f0a8b9d9dc03628821e180266fe2cbbf
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-07 15:24:32 -0300

    Include swagger docs for user endpoints

--- routes/user_r.js
+++ routes/user_r.js
@@ -2,7 +2,91 @@ const express = require('express');
 const router = new express.Router();
 const usersController = require('../controllers/users_c.js');
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Add a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fname:
+ *                 type: string
+ *                 example: "John"
+ *               lname:
+ *                 type: string
+ *                 example: "Doe"
+ *               email:
+ *                 type: string
+ *                 example: "JD@test.com"
+ *               username:
+ *                 type: string
+ *                 example: "JD"
+ *               password:
+ *                 type: string
+ *                 example: "123"
+ *               state:
+ *                 type: string
+ *                 example: "Utah"
+ *               country:
+ *                 type: string
+ *                 example: "USA"
+ *     responses:
+ *       200:
+ *         description: User added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: "12345"
+ *                 fname:
+ *                   type: string
+ *                   example: "John"
+ *                 lname:
+ *                   type: string
+ *                   example: "Doe"
+ *                 email:
+ *                   type: string
+ *                   example: "JD@test.com"
+ *                 username:
+ *                   type: string
+ *                   example: "JD"
+ *                 state:
+ *                   type: string
+ *                   example: "Utah"
+ *                 country:
+ *                   type: string
+ *                   example: "USA"
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', usersController.addUser);
-router.delete('./:username', usersController.deleteUserByName);
+
+/**
+ * @swagger
+ * /users/{username}:
+ *   delete:
+ *     summary: Delete a user by username
+ *     parameters:
+ *       - in: path
+ *         name: username
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The username of the user to delete
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ */
+router.delete('/:username', usersController.deleteUserByName);
 
 module.exports = router;
