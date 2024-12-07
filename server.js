626c15e9fc824cd4db7f73b7365bc2e5b843739a
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-07 15:32:15 -0300

    Add endpoint protection

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 const router = express.Router();
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const route = require('./routes');
 const mongoDb = require('./database/data')
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests from this IP address. Please try again after an hour.',
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,6 +19,8 @@ app.get('/', (req, res) => {
     res.send('API is running');
 });
 
+// Protect all routes with rate limiter to prevent abuse
+app.use('/', limiter);
 app.use('/', route);
 
 mongoDb.initDb((err) => {
