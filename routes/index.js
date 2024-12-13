2242b57f4be3b8e8b3ae1978f9508b0a36a9e65b
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-13 11:05:31 -0300

    Remove unused variable

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = new express.Router();
-const db = require('../database/data');
 
 router.use('/animals', require('./animal_r'));
 router.use('/users', require('./user_r'));
