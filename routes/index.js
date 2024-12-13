b1000323d63c8a2a1c1e4259c50d03a4d614bb52
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-13 11:09:34 -0300

    Add login and logout routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = new express.Router();
+const passport = require('passport');
+
+// eslint-disable-next-line no-unused-vars
+router.get('/login', passport.authenticate('github'), (_req, _res) => {});
+
+router.get('/logout', (req, res, next) => {
+  req.logout(err => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
 
 router.use('/animals', require('./animal_r'));
 router.use('/users', require('./user_r'));
