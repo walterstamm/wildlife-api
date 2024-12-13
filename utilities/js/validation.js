92f28fb5fa9f0d3188204dce375b17123fe1c95d
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-13 10:51:00 -0300

    Fix typo on body method

--- utilities/js/validation.js
+++ utilities/js/validation.js
@@ -108,7 +108,7 @@ validate.reportRules = () => {
     body('observation_id').isMongoId().withMessage('Please provide the id of the associated observation.'),
     body('date').trim().escape().matches(/^20\d{2}-(0[1-9]|1[0-2])-[0-3]\d$/).withMessage('Please include the date of the report, in the form yyyy-mm-dd.'),
     body('time').trim().escape().custom(value => ['morning', 'afternoon', 'evening', 'night'].includes(value)).withMessage('Please provide the time of day of the report. Accepted values are "morning", "afternoon", "evening", and "night".'),
-    body('weather').trim().escape().length({min:1}).withMessage("Please provide the weather at the time of the report.")
+    body('weather').trim().escape().isLength({ min: 1 }).withMessage("Please provide the weather at the time of the report.")
   ]
 };
 
