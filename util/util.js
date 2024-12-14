9363bd37f0323d400414f267afe74ce903623a52
Ganonymous <andrewleetham@gmail.com>
2024-12-14 12:49:33 -0800

    Prevent empty strings, ensure cross-collection ids exist.

--- util/util.js
+++ util/util.js
@@ -1,5 +1,28 @@
+const db = require('../database/data');
+const ObjectId = require('mongodb').ObjectId;
+
 const handleErrors = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-module.exports = { handleErrors };
+const idInDatabase = async function(id, collectionName){
+    try{
+        const database = await db.getDatabase();
+
+        const idString = String(id);
+        const target = new ObjectId(idString);
+
+        const collection = database.db('WildlifeAPI').collection(collectionName);
+        const matches = await collection.countDocuments({_id: target});
+        if(matches > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    } catch{
+        return false;
+    }
+}
+
+module.exports = { handleErrors, idInDatabase };
--- utilities/js/validation.js
+++ utilities/js/validation.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 const { body, param, validationResult } = require('express-validator');
+const {idInDatabase} = require('../../util/util.js');
 const validate = {};
 
 validate.idRule = () => {
@@ -25,10 +26,10 @@ validate.checkId = (req, res, next) => {
 
 validate.animalRules = () => {
     return [
-        body('category').trim().escape().isLength({ min: 1 }).withMessage('Please provide a category.'),
-        body('common_name').trim().escape().isLength({ min: 1 }).withMessage('Please provide the common name.'),
-        body('diet').trim().escape().isLength({ min: 1 }).withMessage('Please include the animal\'s diet.'),
-        body('scientific_name').trim().escape().isLength({ min: 1 }).withMessage('Please provide the scientific name.')
+        body('category').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide a category.'),
+        body('common_name').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide the common name.'),
+        body('diet').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please include the animal\'s diet.'),
+        body('scientific_name').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide the scientific name.')
     ]
 };
 
@@ -50,9 +51,9 @@ ${errorString}`);
 
 validate.userRules = () => {
   return [
-    body('githubId').trim().escape().isLength({ min: 1 }).withMessage('Please provide a valid GitHub id.'),
-    body('username').trim().escape().isLength({ min: 1 }).withMessage('Please provide a valid GitHub username.'),
-    body('displayName').trim().escape().isLength({ min: 1 }).withMessage('Please provide a valid GitHub display name.'),
+    body('githubId').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide a valid GitHub id.'),
+    body('username').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide a valid GitHub username.'),
+    body('displayName').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide a valid GitHub display name.'),
     body('profileUrl').trim().isURL().withMessage('Please provide a valid GitHub profile url.'),
   ]
 };
@@ -77,10 +78,10 @@ validate.checkUser = (req, res, next) => {
 
 validate.observationRules = () => {
   return [
-      body('animal_id').isMongoId().withMessage('Please provide the id of the animal observed.'),
+      body('animal_id').isMongoId().custom((value) => idInDatabase(value, 'Animals')).withMessage('Please provide the id of the animal observed.'),
       body('age').trim().escape().custom(value => ['juvenile', 'adolescent', 'adult', 'elderly'].includes(value)).withMessage('Please provide the age of the observed animal. Accepted values are "juvenile", "adolescent", "adult", and "elderly".'),
       body('gender').trim().escape().custom(value => ['male', 'female', 'unknown'].includes(value)).withMessage('Please provide the gender of the observed animal. Accepted values are "male", "female", and "unknown".'),
-      body('behavior').trim().escape().isLength({ min: 1 }).withMessage('Please provide what the observed animal was doing.')
+      body('behavior').trim().escape().notEmpty().isLength({ min: 1 }).withMessage('Please provide what the observed animal was doing.')
   ]
 };
 
@@ -102,11 +103,11 @@ ${errorString}`);
 
 validate.reportRules = () => {
   return [
-    body('user_id').isMongoId().withMessage('Please provide the id of the user making the report.'),
-    body('observation_id').isMongoId().withMessage('Please provide the id of the associated observation.'),
+    body('user_id').isMongoId().custom((value) => idInDatabase(value, 'Users')).withMessage('Please provide the id of the user making the report.'),
+    body('observation_id').isMongoId().custom((value) => idInDatabase(value, 'Observations')).withMessage('Please provide the id of the associated observation.'),
     body('date').trim().escape().matches(/^20\d{2}-(0[1-9]|1[0-2])-[0-3]\d$/).withMessage('Please include the date of the report, in the form yyyy-mm-dd.'),
     body('time').trim().escape().custom(value => ['morning', 'afternoon', 'evening', 'night'].includes(value)).withMessage('Please provide the time of day of the report. Accepted values are "morning", "afternoon", "evening", and "night".'),
-    body('weather').trim().escape().isLength({ min: 1 }).withMessage("Please provide the weather at the time of the report.")
+    body('weather').trim().escape().notEmpty().isLength({ min: 1 }).withMessage("Please provide the weather at the time of the report.")
   ]
 };
 
