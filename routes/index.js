1ce4180c74a86313c4dd1ed4281caa2f015ddca6
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-13 21:19:49 -0300

    Document login and logout routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,18 @@ const router = new express.Router();
 const passport = require('passport');
 
 // eslint-disable-next-line no-unused-vars
-router.get('/login', passport.authenticate('github'), (_req, _res) => {});
+router.get('/login', passport.authenticate('github'), (_req, _res) => {
+  // The request will be redirected to GitHub for authentication, so this
+  // function will not be called.
+  // #swagger.tags = ['General']
+  // #swagger.responses[200] = {description: "Success"}
+  // #swagger.responses[500] = {description: "Internal Server Error"}
+});
 
 router.get('/logout', (req, res, next) => {
+  // #swagger.tags = ['General']
+  // #swagger.responses[200] = {description: "Success"}
+  // #swagger.responses[500] = {description: "Internal Server Error"}
   req.logout(err => {
     if (err) {
       return next(err);
