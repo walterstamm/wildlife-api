8d92849fb058bd207c134a6e7a4135e35c82fbea
Gabriel Cánepa <gacanepa@gmail.com>
2024-12-13 21:20:30 -0300

    Include General section in the docs

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -12,6 +12,10 @@ const doc = {
   produces: ['application/json'],
   tags: [
     {
+      name: 'General',
+      description: 'Endpoints for general operations like login and logout',
+    },
+    {
       name: 'Animals',
       description: 'Endpoints for animals in the database',
     },
